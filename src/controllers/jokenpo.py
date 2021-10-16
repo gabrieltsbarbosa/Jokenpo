1a9e8654e885f9874b70cda51df8d1bbe178cb3d
gabrieltsbarbosa <gabrieltsbarbosa@hotmail.com>
2021-10-16 20:28:51 -0300

    jokempo.py changed to pep8 pattern

--- src/controllers/jokenpo.py
+++ src/controllers/jokenpo.py
@@ -10,30 +10,51 @@ application, api = server.application, server.api
 @api.route('/jokenpo')
 class Jokenpo(Resource):
     @application.route('/health', methods=['GET'])
-    def healthCheck():
+    def healthCheck() -> str:
+        """
+        Use to check application health
+        """
         return 'healthCheck', 200
 
     @application.route('/jokenpo', methods=['GET'])
-    def get():
+    def get() -> dict:
+        """
+        call this to get game result
+        """
         return Repo.get_result()
 
     @application.route('/jokenpo/player/<int:id>', methods=['GET'])
-    def get_player(id):
+    def get_player(id: int) -> dict:
+        """
+        Use to get player name and move
+        """
         return Repo.get_player(id)
     
     @application.route('/jokenpo/player', methods=['POST'])
-    def post():
+    def post() -> dict:
+        """
+        Use to insert a player into db
+        """
         return Repo.insert_player(api.payload)
 
     @application.route('/jokenpo/player/<int:id>', methods=['PUT'])
-    def put(id):
+    def put(id) -> str:
+        """
+        Use to change player move
+        """
         return Repo.update_move(id, api.payload)
     
     @application.route('/jokenpo/player/<int:id>', methods=['DELETE'])
-    def delete(id):
+    def delete(id: int) -> str:
+        """
+        Use to delete a player
+        """
         return Repo.deletePlayer(id)
 
     
     @application.route('/jokenpo/player/all', methods=['DELETE'])
-    def delete_all():
+    def delete_all() -> str:
+        """
+        Use to clear all players
+        """
         return Repo.delete_all_players()
